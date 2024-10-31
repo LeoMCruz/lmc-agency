17a2eca8e19e023cad562cc2dc60ae269fe1d1c1
Leonardo Cruz <leomcruzz@gmail.com>
2024-10-31 10:35:34 -0300

    Finished js for login validation

---
+++ javaScript/login.js
@@ -0,0 +1,50 @@
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let form = document.querySelector('form');
+let loginMessage = document.getElementById('loginMessage');
+
+
+function emailCheck(email){
+    let emailPattern =
+    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    return emailPattern.test(email);
+}
+
+function passwordCheck(password) {
+    let passwordPattern = /^[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+    return passwordPattern.test(password);
+}
+
+function clearMessage(){
+    loginMessage.textContent = "";
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    clearMessage;    
+});
+
+function inputsCheck() {
+    const emailValid = emailCheck(email.value);
+    const passwordValid = passwordCheck(password.value);
+
+    if (email.value === "" || password.value === "") {
+        loginMessage.textContent = "Todos os campos são obrigatórios";
+        loginMessage.style.color = "red";
+    } else if (!emailValid && !passwordValid) {
+        loginMessage.textContent = "Email ou senha inválidos";
+        loginMessage.style.color = "red";
+    } else if (!emailValid) {
+        loginMessage.textContent = "Email ou senha inválidos";
+        loginMessage.style.color = "red";
+    } else if (!passwordValid) {
+        loginMessage.textContent = "A senha deve ter no mínimo 8 caracteres";
+        loginMessage.style.color = "red";
+    } else {
+        loginMessage.textContent = "Dados Ok";
+        loginMessage.style.color = "green";
+    }
+}
+
+email.addEventListener('keyup', inputsCheck);
+password.addEventListener('keyup', inputsCheck);
