901317862afd30085308607d097b4ca808e5181f
Leonardo Cruz <leomcruzz@gmail.com>
2024-11-01 10:55:40 -0300

    Created dynamic modal for Cards

--- javaScript/home.js
+++ javaScript/home.js
@@ -3,12 +3,33 @@ function feedCards(){
         .then(response => response.json())
         .then(items => {
             const containerCards = document.getElementById('container-cards');
+            const modal = document.getElementById('modal');
+            const modalSize = document.getElementById('modal-size');
+            const modalBedrooms = document.getElementById('modal-bedrooms');
+            const modalBathrooms = document.getElementById('modal-bathrooms');
+            const modalPrice = document.getElementById('modal-price');
+            const modalImage = document.getElementById('modal-image');
+            const closeModal = document.getElementById('close-modal');
             items.map(containerCard => {
                 const card = document.createElement("div");
                 card.classList.add("card");
 
                 const img = document.createElement("img");
                 img.src = containerCard.image;
+                img.addEventListener('click', function() {
+                    modalImage.src = containerCard.image;
+                    modalPrice.textContent = `${containerCard.price}`;
+                    modalPrice.classList.add("#modal-price");
+                    modalSize.textContent = `${containerCard.size} m²`;
+                    modalBedrooms.textContent = `${containerCard.bedrooms} Quartos `;
+                    modalBedrooms.classList.add("rooms-gray");
+                    modalBathrooms.textContent = `${containerCard.bathrooms} Banheiros`;
+                    modalBathrooms.classList.add("rooms-gray");
+                    modal.showModal();
+                    modal.addEventListener('click', function(e) {
+                        if (e.target == this) modal.close();
+                      });
+                });
 
                 const price = document.createElement("h3");
                 price.textContent = containerCard.price;
@@ -32,6 +53,9 @@ function feedCards(){
 
                 containerCards.appendChild(card);
             })
+            closeModal.addEventListener('click', function() {
+                modal.close();
+            });
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
@@ -41,3 +65,4 @@ function feedCards(){
 }
 
 feedCards();
+
